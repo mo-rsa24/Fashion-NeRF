feb326398253c319d8bb3e1d258955786d848992
Molefe <1858893@students.wits.ac.za>
2024-02-13 19:26:09 +0200

    Griffin parser_free warp changes

--- debug/fs_vton/pf_fsvton_warp.sh
+++ debug/fs_vton/pf_fsvton_warp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export EXPERIMENT_NUMBER=34
+export EXPERIMENT_NUMBER=48
 export EXPERIMENT_FROM_NUMBER=0
 export RUN_NUMBER=99
 export RUN_FROM_NUMBER=0
@@ -15,8 +15,8 @@ export VITON_NAME=FS_VTON
 
 ./scripts/viton/viton.sh --job_name $VITON_NAME --task $TASK --experiment_number $EXPERIMENT_NUMBER --run_number $RUN_NUMBER \
     --experiment_from_number 0 --run_from_number 0 \
-    --parser_based_warp_experiment_from_number 30 --parser_based_warp_run_from_number 21 --warp_load_from_model Rail \
-    --parser_based_gen_experiment_from_number 32 --parser_based_gen_run_from_number 21 --gen_load_from_model Rail \
+    --parser_based_warp_experiment_from_number 30 --parser_based_warp_run_from_number 29 --warp_load_from_model Rail \
+    --parser_based_gen_experiment_from_number 32 --parser_based_gen_run_from_number 30 --gen_load_from_model Rail \
     --parser_free_warp_experiment_from_number 0 --parser_free_warp_run_from_number 0 --parser_free_warp_load_from_model Original \
     --dataset_name $DATASET_NAME --device $DEVICE --load_last_step False --run_wandb $WANDB \
     --niter 50 --niter_decay 50 --display_count 1 --print_step 1 --save_period 1 --val_count 1 \
